dd3e2165a0f521895b676694d0b4a1de56dde9c8
idris <idrisdoyin@gmail.com>
2022-04-16 13:42:20 +0100

    email fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                 <SimpleGrid columns={{base:1, md:3}} spacing={4} p={{base:'20px', md:'150px'}}  bg='darkgreen'>
                 <Flex direction='column'>
                         <Text fontSize={{base:'14px', md:'16px'}} fontWeight='500' color='disable'>DM us on:</Text>
-                        <Text fontSize={{base:'18px', md:'24px'}} fontWeight='400' color='white'>rennyoni@yahoo.com</Text>
+                        <Text fontSize={{base:'18px', md:'24px'}} fontWeight='400' color='white'>campaign@yemiosinbajo.ng</Text>
                 </Flex>
                 <Flex direction='column'>
                         <Text fontSize={{base:'14px', md:'16px'}} fontWeight='500' color='disable'>Call us on:</Text>
@@ -30,7 +30,7 @@ const Footer = () => {
                 <Divider borderColor='disable'/>
                     <Flex direction={{base:'column-reverse', md:'row'}} gap={4} px={{base:'20px', md:'150px'}} py={{base:6, md:12}} bg='darkgreen'>
                         <Flex TextAlign='center' w={{base:'100%', md:'50%'}}>
-                                <Text fontSize={{base:'14px', md:'18px'}} fontWeight='400' color='disable'>©Osinbajo 2021. All rights reserved.</Text>
+                                <Text fontSize={{base:'14px', md:'18px'}} fontWeight='400' color='disable'>©Osinbajo 2023. All rights reserved.</Text>
                         </Flex>
                         <Spacer/>
                         <Flex gap={12} w={{base:'100%', md:'50%'}} justifyContent='flex-end'>
